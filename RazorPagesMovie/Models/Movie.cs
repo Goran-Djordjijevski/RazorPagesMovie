c22c77f7d6fb5fdfcda721761ae6bbf8ecf6d38e
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-05-06 14:47:24 +0200

    Update pages with new code.

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,8 @@ namespace RazorPagesMovie.Models
 
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
         public string Genre { get; set; }
