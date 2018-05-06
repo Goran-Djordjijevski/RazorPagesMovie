8ba3ced577bac49cda548cc1af974ea79f713c88
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-05-06 16:18:46 +0200

    Add validation to Movie.cs file.

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -10,16 +10,26 @@ namespace RazorPagesMovie.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(maximumLength: 60, MinimumLength = 3)]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
 
+        [Range(minimum: 1, maximum: 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
     }
 }
